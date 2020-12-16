dc13510203d564394e2b0a9aae817fb391c8dd3a
MOHAMMED BELACHHAB <Belmedbsns@gmail.com>
2020-12-16 16:08:25 +0100

    I add the patient potentiel class

---
+++ Covide_19_Tracker/PatientPotentiel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Covide_19_Tracker
+{
+    public class PatientPotentiel
+    {
+        public bool TestResult
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}
