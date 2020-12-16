8daab6df5f532e1090a19e5988a44deedffe76f1
MOHAMMED BELACHHAB <Belmedbsns@gmail.com>
2020-12-16 15:38:42 +0100

    I add the citoyen class

---
+++ Covide_19_Tracker/Citoyen.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Covide_19_Tracker
+{
+    public class Citoyen
+    {
+        private int nom;
+        private int prenom;
+        private int cni;
+        private int adress;
+        private int date;
+        private int genre;
+        private int phone;
+        private int field;
+        private int statut;
+
+        public int Adress
+        {
+            get ;
+            set;
+            
+        }
+
+        public int Cni
+        {
+            get ;
+            set;
+            
+        }
+
+        public int Date
+        {
+            get;
+            set;
+            
+        }
+
+        public int Genre
+        {
+            get ;
+            set;
+            
+        }
+
+        public int Nom
+        {
+            get;
+            set;
+        }
+
+        public int Phone
+        {
+            get;
+            set;
+            
+        }
+
+        public int Prenom
+        {
+            get;
+            set;
+           
+        }
+
+        public int Statut
+        {
+            get;
+            set;
+            
+        }
+    
+    }
+}
