68d6e6ea2ab8e882f91d0d7ce9af29abf0cb42a1
MOHAMMED BELACHHAB <Belmedbsns@gmail.com>
2020-12-16 15:50:29 +0100

    I add the patient class

---
+++ Covide_19_Tracker/Patient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Covide_19_Tracker
+{
+    public class Patient
+    {
+        public string Letat
+        {
+            get ;
+            set ;
+            
+        }
+    }
+}
