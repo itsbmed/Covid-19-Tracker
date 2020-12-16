4165ac9922e14415708f39debe5f13b4ffd97740
MOHAMMED BELACHHAB <Belmedbsns@gmail.com>
2020-12-16 16:01:04 +0100

    I add the analysis laboratory class

---
+++ Covide_19_Tracker/Laboratoir.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Covide_19_Tracker
+{
+    public class Laboratoir
+    {
+        public string Name
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public string Adress
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public bool Result()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
