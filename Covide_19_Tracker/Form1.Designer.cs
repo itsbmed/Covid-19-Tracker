1da5aa4a2000979be15ed5ae492dfdecab7f0ae8
MOHAMMED BELACHHAB <Belmedbsns@gmail.com>
2020-12-26 22:50:16 +0100

    Sign Up Design Updates

--- Covide_19_Tracker/Form1.Designer.cs
+++ Covide_19_Tracker/Form1.Designer.cs
@@ -39,6 +39,8 @@ namespace Covide_19_Tracker
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
             this.checkedListBox2 = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -53,51 +55,61 @@ namespace Covide_19_Tracker
             // 
             // textBox1
             // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(509, 299);
+            this.textBox1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.textBox1.Font = new System.Drawing.Font("Rajdhani", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.Info;
+            this.textBox1.Location = new System.Drawing.Point(509, 351);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(385, 29);
+            this.textBox1.Size = new System.Drawing.Size(385, 32);
             this.textBox1.TabIndex = 1;
             this.textBox1.Text = "Phone";
             this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // textBox2
             // 
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(509, 391);
+            this.textBox2.BackColor = System.Drawing.SystemColors.Highlight;
+            this.textBox2.Font = new System.Drawing.Font("Rajdhani", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ForeColor = System.Drawing.SystemColors.Info;
+            this.textBox2.Location = new System.Drawing.Point(509, 415);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(385, 29);
+            this.textBox2.Size = new System.Drawing.Size(385, 32);
             this.textBox2.TabIndex = 2;
             this.textBox2.Text = "Brithday Date";
             this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // textBox4
             // 
-            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox4.Location = new System.Drawing.Point(509, 128);
+            this.textBox4.BackColor = System.Drawing.SystemColors.Highlight;
+            this.textBox4.Font = new System.Drawing.Font("Rajdhani", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.ForeColor = System.Drawing.SystemColors.Info;
+            this.textBox4.Location = new System.Drawing.Point(509, 223);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(385, 29);
+            this.textBox4.Size = new System.Drawing.Size(385, 32);
             this.textBox4.TabIndex = 4;
             this.textBox4.Text = "Cni";
             this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // textBox5
             // 
-            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox5.Location = new System.Drawing.Point(509, 212);
+            this.textBox5.BackColor = System.Drawing.SystemColors.Highlight;
+            this.textBox5.Font = new System.Drawing.Font("Rajdhani", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.ForeColor = System.Drawing.SystemColors.Info;
+            this.textBox5.Location = new System.Drawing.Point(509, 282);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(385, 29);
+            this.textBox5.Size = new System.Drawing.Size(385, 32);
             this.textBox5.TabIndex = 5;
             this.textBox5.Text = "Adress";
             this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // textBox6
             // 
-            this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox6.Location = new System.Drawing.Point(509, 47);
+            this.textBox6.BackColor = System.Drawing.SystemColors.Highlight;
+            this.textBox6.Font = new System.Drawing.Font("Rajdhani", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox6.ForeColor = System.Drawing.SystemColors.Info;
+            this.textBox6.Location = new System.Drawing.Point(509, 161);
             this.textBox6.Margin = new System.Windows.Forms.Padding(12);
             this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(385, 29);
+            this.textBox6.Size = new System.Drawing.Size(385, 32);
             this.textBox6.TabIndex = 6;
             this.textBox6.Text = "Full Name";
             this.textBox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
@@ -106,9 +118,9 @@ namespace Covide_19_Tracker
             // 
             this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(3, 0);
+            this.pictureBox1.Location = new System.Drawing.Point(-1, -7);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(449, 740);
+            this.pictureBox1.Size = new System.Drawing.Size(449, 757);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 9;
             this.pictureBox1.TabStop = false;
@@ -116,18 +128,24 @@ namespace Covide_19_Tracker
             // checkedListBox1
             // 
             this.checkedListBox1.AllowDrop = true;
+            this.checkedListBox1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.checkedListBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.checkedListBox1.ForeColor = System.Drawing.SystemColors.Info;
             this.checkedListBox1.FormattingEnabled = true;
             this.checkedListBox1.Items.AddRange(new object[] {
             "Normal",
             "Patient"});
-            this.checkedListBox1.Location = new System.Drawing.Point(740, 489);
+            this.checkedListBox1.Location = new System.Drawing.Point(783, 489);
             this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(154, 64);
+            this.checkedListBox1.Size = new System.Drawing.Size(111, 60);
             this.checkedListBox1.TabIndex = 10;
             // 
             // checkedListBox2
             // 
             this.checkedListBox2.AllowDrop = true;
+            this.checkedListBox2.BackColor = System.Drawing.SystemColors.Highlight;
+            this.checkedListBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.checkedListBox2.ForeColor = System.Drawing.SystemColors.Info;
             this.checkedListBox2.FormattingEnabled = true;
             this.checkedListBox2.Items.AddRange(new object[] {
             "Man",
@@ -135,14 +153,41 @@ namespace Covide_19_Tracker
             "Kid"});
             this.checkedListBox2.Location = new System.Drawing.Point(509, 489);
             this.checkedListBox2.Name = "checkedListBox2";
-            this.checkedListBox2.Size = new System.Drawing.Size(142, 64);
+            this.checkedListBox2.Size = new System.Drawing.Size(142, 60);
             this.checkedListBox2.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Adventure Hollow", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(575, 20);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(12);
+            this.label1.Size = new System.Drawing.Size(269, 70);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Hey, Welcome";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Rajdhani", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(491, 100);
+            this.label2.Name = "label2";
+            this.label2.Padding = new System.Windows.Forms.Padding(12);
+            this.label2.Size = new System.Drawing.Size(427, 54);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Please Enter Your Personel Informations";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // SignUp
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
             this.ClientSize = new System.Drawing.Size(949, 741);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.checkedListBox2);
             this.Controls.Add(this.checkedListBox1);
             this.Controls.Add(this.pictureBox1);
@@ -171,6 +216,8 @@ namespace Covide_19_Tracker
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.CheckedListBox checkedListBox1;
         private System.Windows.Forms.CheckedListBox checkedListBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
