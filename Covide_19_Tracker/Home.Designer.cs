5c414872ce48663c8c9856faaf2e1c569ea135b7
MOHAMMED BELACHHAB <Belmedbsns@gmail.com>
2021-01-12 10:47:23 +0100

    Updates

---
+++ Covide_19_Tracker/Home.Designer.cs
@@ -0,0 +1,214 @@
+﻿
+namespace Covide_19_Tracker
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.Searchbox = new System.Windows.Forms.TextBox();
+            this.Vaccine = new System.Windows.Forms.Label();
+            this.Patient = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Username = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.BackgroundImage = global::Covide_19_Tracker.Properties.Resources.SI;
+            this.SearchButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.SearchButton.Location = new System.Drawing.Point(664, 229);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(39, 36);
+            this.SearchButton.TabIndex = 15;
+            this.SearchButton.UseVisualStyleBackColor = true;
+            // 
+            // Searchbox
+            // 
+            this.Searchbox.Font = new System.Drawing.Font("Goldman", 18F);
+            this.Searchbox.Location = new System.Drawing.Point(416, 229);
+            this.Searchbox.Name = "Searchbox";
+            this.Searchbox.Size = new System.Drawing.Size(252, 36);
+            this.Searchbox.TabIndex = 14;
+            // 
+            // Vaccine
+            // 
+            this.Vaccine.AutoSize = true;
+            this.Vaccine.Font = new System.Drawing.Font("Rajdhani", 18F, System.Drawing.FontStyle.Bold);
+            this.Vaccine.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Vaccine.Location = new System.Drawing.Point(78, 184);
+            this.Vaccine.Name = "Vaccine";
+            this.Vaccine.Size = new System.Drawing.Size(99, 30);
+            this.Vaccine.TabIndex = 17;
+            this.Vaccine.Text = "Vaccine :";
+            // 
+            // Patient
+            // 
+            this.Patient.AutoSize = true;
+            this.Patient.Font = new System.Drawing.Font("Rajdhani", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Patient.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Patient.Location = new System.Drawing.Point(82, 141);
+            this.Patient.Name = "Patient";
+            this.Patient.Size = new System.Drawing.Size(95, 30);
+            this.Patient.TabIndex = 16;
+            this.Patient.Text = "Patient :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Rajdhani", 18F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(78, 229);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 30);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Possible :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Rajdhani", 18F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(82, 273);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 30);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Normale :";
+            // 
+            // Username
+            // 
+            this.Username.AutoSize = true;
+            this.Username.Font = new System.Drawing.Font("Bangers", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Username.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Username.Location = new System.Drawing.Point(192, 23);
+            this.Username.Name = "Username";
+            this.Username.Size = new System.Drawing.Size(453, 56);
+            this.Username.TabIndex = 20;
+            this.Username.Text = " Hey, Welcome To Covid-19-Tracker";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Rajdhani", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(222, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 30);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "1500";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Rajdhani", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.Location = new System.Drawing.Point(222, 184);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 30);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "120";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Rajdhani", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label5.Location = new System.Drawing.Point(222, 229);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 30);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "131";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Rajdhani", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label6.Location = new System.Drawing.Point(222, 273);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 30);
+            this.label6.TabIndex = 24;
+            this.label6.Text = "1865";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Rajdhani", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label7.Location = new System.Drawing.Point(411, 156);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(222, 30);
+            this.label7.TabIndex = 25;
+            this.label7.Text = "Search For A Patient :";
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Username);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Vaccine);
+            this.Controls.Add(this.Patient);
+            this.Controls.Add(this.SearchButton);
+            this.Controls.Add(this.Searchbox);
+            this.Name = "Home";
+            this.Text = "Home";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SearchButton;
+        private System.Windows.Forms.TextBox Searchbox;
+        private System.Windows.Forms.Label Vaccine;
+        private System.Windows.Forms.Label Patient;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Username;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
